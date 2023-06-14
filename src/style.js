6f75f6c062d48b0fbd0e52e639de428b33a1d004
Leo Tomas <tomasl@rcvs.org.uk>
2023-06-14 17:52:57 +0100

    feat: changing modal padding/border radius/bottom margin

--- src/style.js
+++ src/style.js
@@ -2,6 +2,7 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
   wrapper: {
+    padding: 20,
     flex: 1,
     backgroundColor: "#00000077"
   },
@@ -11,8 +12,10 @@ const styles = StyleSheet.create({
   },
   container: {
     backgroundColor: "#fff",
+    borderRadius: 10,
     width: "100%",
     height: 0,
+    marginBottom: 20,
     overflow: "hidden"
   },
   draggableContainer: {
